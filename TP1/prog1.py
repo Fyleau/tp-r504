b76ee988153ff3b7e236a6bbace9c0ba409f12da
paquetom <geekrato.gk@gmail.com>
2026-09-09 17:42:21 +0200

    ajout de fonctions.py

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,1 +1,12 @@
 print("Hello, World!")
+
+import fonctions as f
+
+while True:
+	a = input("Entrez le premier nombre : ")
+	b = input("Entrez le deuxième nombre : ")
+
+	res = f.puissance(a, b)
+
+print("Résultat :", res)
+
